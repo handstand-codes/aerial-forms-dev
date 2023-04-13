adb96f2881ead4ccb8af969ae6cb6922bad21480
Chester Vincent <chet.vincent@gmail.com>
2023-04-13 08:57:05 -0400

    updates

--- web/frontend/assets/index.js
+++ web/frontend/assets/index.js
@@ -1,3 +1,5 @@
 export { default as notFoundImage } from "./empty-state.svg";
 export { default as trophyImage } from "./home-trophy.png";
 export { default as klaviyoLogo } from "./klaviyo.png";
+export { default as shopifyLogo } from "./shopify.png";
+export { default as mailchimpLogo } from "./mailchimp.png";
--- web/frontend/pages/integrations.jsx
+++ web/frontend/pages/integrations.jsx
@@ -3,11 +3,10 @@ import { api } from "../api";
 import { useFindMany } from "@gadgetinc/react";
 import { useAction } from "@gadgetinc/react";
 import { useNavigate } from "@shopify/app-bridge-react";
-import { klaviyoLogo } from "../assets";
+import { klaviyoLogo, shopifyLogo, mailchimpLogo } from "../assets";
 
 import { 
     Page,
-    Layout,
     CalloutCard,
     Card,
     Checkbox,
@@ -25,23 +24,9 @@ export default function IntegrationsPage() {
     const navigate = useNavigate();
 
     const [data, setData] = useState("");
-    const [checked, setChecked] = useState();
-    const [isFirstButtonActive, setIsFirstButtonActive] = useState(true);
-    const [shopifyChecked, setShopifyChecked] = useState();
-    const [listId, setListId] = useState('');
-    const [apiKey, setApiKey] = useState('');
     
-
-    const handleListIdChange = useCallback((
-    newListId
-    ) => setListId(newListId), []);
-
-    const handleApiKeyChange = useCallback((
-    newApiKey
-    ) => setApiKey(newApiKey), []);
-
-
     // Get //
+    
     const [klaviyoRecords] = useFindMany(api.klaviyo, {    
         filter: {
           currentStoreId: {
@@ -57,12 +42,45 @@ export default function IntegrationsPage() {
             },
           }
       });
+
+    const [mailchimpEnabled] = useFindMany(api.mailchimpEnabled, {    
+        filter: {
+            currentStoreId: {
+              equals: data.currentShopId,
+            },
+          }
+      });
+
+      
       
     // API Calls //
     const [klaviyoResponse, updateKlaviyo] = useAction(api.klaviyo.update);
     const [shopifyEnabledResponse, updateShopifyEnabled] = useAction(api.shopifyEnabled.update);
+    const [mailchimpEnabledResponse, updateMailchimpEnabled] = useAction(api.mailchimpEnabled.update);
+
+
+    // Update Shopify Enabled  //
+
+    const saveShopifyCheck = useCallback(
+        async (id, enabled) => {
+            const changed = !enabled
+            setShopifyChecked(changed)
+            const shopifyEnabled = 
+                {
+                enabled: changed
+                }               
+    await updateShopifyEnabled({ id, shopifyEnabled });            
+        }
+    );  
+
+    if (shopifyEnabledResponse.fetching || shopifyEnabledResponse.data) {
+        if (shopifyEnabledResponse.data) {
+        navigate("/integrations");
+        }
+    };
 
     // Update Klaviyo Enabled //
+
     const saveCheck = useCallback(
         async (id, enabled) => {
             const changed = !enabled
@@ -76,22 +94,8 @@ export default function IntegrationsPage() {
         }
       );  
 
-    // Update Shopify Enabled  //
-    const saveShopifyCheck = useCallback(
-        async (id, enabled) => {
-            const changed = !enabled
-            setShopifyChecked(changed)
-            const shopifyEnabled = 
-                {
-                enabled: changed
-                }          
-        
-      await updateShopifyEnabled({ id, shopifyEnabled });  
-           
-    }
-  );  
-
     // Update List ID and API Key //
+
     const saveInfo = useCallback(
     async (id) => {
         const klaviyo = 
@@ -101,23 +105,50 @@ export default function IntegrationsPage() {
         }          
         console.log(klaviyo)
         await updateKlaviyo({ id, klaviyo });         
-    }
+        }
     );  
 
     if (klaviyoResponse.fetching || klaviyoResponse.data) {
-    if (klaviyoResponse.data) {
+        if (klaviyoResponse.data) {
         navigate("/integrations");
         }
-    }
+    };
 
-    if (shopifyEnabledResponse.fetching || shopifyEnabledResponse.data) {
-        if (shopifyEnabledResponse.data) {
-            navigate("/integrations");
-            }
+    // Update Mailchimp Enabled  //
+
+    const saveMailchimpCheck = useCallback(
+        async (id, enabled) => {
+            const changed = !enabled
+            setMailchimpChecked(changed)
+            const mailchimpEnabled = 
+                {
+                enabled: changed
+                }               
+    await updateMailchimpEnabled({ id, mailchimpEnabled });            
         }
+    );  
 
+    // Update List ID and API Key //
+
+    const saveMailchimpInfo = useCallback(
+        async (id) => {
+            const mailchimpEnabled = 
+            {
+                apiKey: apiKey,
+                server: server
+            }               
+            await updateMailchimpEnabled({ id, mailchimpEnabled });         
+            }
+        );  
+
+    if (mailchimpEnabledResponse.fetching || mailchimpEnabledResponse.data) {
+        if (mailchimpEnabledResponse.data) {
+        navigate("/integrations");
+        }
+    };
 
     // USE EFFECT //
+
     useEffect(() => {
         klaviyoRecords.data?.map((startState, i) => (
             setChecked(startState.enabled),
@@ -128,6 +159,12 @@ export default function IntegrationsPage() {
         shopifyEnabled.data?.map((startState, i) => (
             setShopifyChecked(startState.enabled)
          ));
+
+         mailchimpEnabled.data?.map((startState, i) => (
+            setMailchimpChecked(startState.enabled),
+            setMailchimpApiKey(startState.apiKey),
+            setMailchimpServer(startState.server)
+         ));
          
         const customHttpRouteRequest = async () => {
           const result = await api.connection.fetch("https://email-getter-v7--development.gadget.app/custom");
@@ -137,15 +174,58 @@ export default function IntegrationsPage() {
         customHttpRouteRequest().catch(console.error);
       }, []);
 
+    // SHOPIFY MARKUP //
 
-    const [showKlaviyo, setShowKlaviyo] = useState(false)
-    
-    const enableKlaviyo = useCallback(
-        () => setShowKlaviyo((showKlaviyo) => !showKlaviyo),
-              
+    const [showShopify, setShowShopify] = useState(false);
+    const [shopifyChecked, setShopifyChecked] = useState();
+
+    const enableShopify = useCallback(
+        () => setShowShopify((showShopify) => !showShopify),       
         []);
-    
 
+    const shopifyMarkup = (
+        showShopify ? (
+            <Grid>
+                <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 3, lg: 6, xl: 6}}>
+                    <Card.Section>
+                        <TextContainer>
+                            {shopifyEnabled.data?.map((enabled, i) => (
+                                <Checkbox
+                                    id={enabled.id}
+                                    key={enabled.id}
+                                    position={i}
+                                    label="Enabled"
+                                    checked={shopifyChecked}
+                                    onChange={() => saveShopifyCheck(enabled.id, enabled.enabled)}
+                                />                                
+                            ))}
+                        </TextContainer>
+                    </Card.Section>                         
+                </Grid.Cell>   
+            </Grid>
+        ):( null )
+    )
+
+    // KLAVIYO MARKUP //
+
+    const [showKlaviyo, setShowKlaviyo] = useState(false); 
+    const [isFirstButtonActive, setIsFirstButtonActive] = useState(true);
+    const [checked, setChecked] = useState();
+    const [listId, setListId] = useState('');
+    const [apiKey, setApiKey] = useState('');
+       
+    const enableKlaviyo = useCallback(
+        () => setShowKlaviyo((showKlaviyo) => !showKlaviyo),       
+        []);
+
+    const handleListIdChange = useCallback((
+        newListId
+        ) => setListId(newListId), []);
+    
+    const handleApiKeyChange = useCallback((
+        newApiKey
+        ) => setApiKey(newApiKey), []);
+    
     const klaviyoMarkup = (
         showKlaviyo ? (
             <Grid>
@@ -182,7 +262,7 @@ export default function IntegrationsPage() {
                 </Grid.Cell> 
 
                 <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 3, lg: 6, xl: 6}}>              
-                    <br/>
+                    
                     <Card.Section>
                     {klaviyoRecords.data?.map((info, i) => (
                     <Form 
@@ -209,20 +289,98 @@ export default function IntegrationsPage() {
                         </FormLayout>
                     </Form>
                     ))}
-                    <br/> 
+                    
                     </Card.Section>                
                 </Grid.Cell>
-                                                     
+                                                
             </Grid>
-
             ):( null )
+    )
+
+    // Mailchimp MARKUP //
+
+    const [showMailchimp, setShowMailchimp] = useState(false);
+    const [mailchimpChecked, setMailchimpChecked] = useState();
+    const [mailchimpApiKey, setMailchimpApiKey] = useState('');
+    const [mailchimpServer, setMailchimpServer] = useState('');
+    
+    const handleMailchimpApiKeyChange = useCallback((
+        newApiKey
+        ) => setMailchimpApiKey(newApiKey), []);
+
+    const handleMailchimpServerIdChange = useCallback((
+        newServer
+        ) => setMailchimpServer(newServer), []);
+
+    const enableMailchimp = useCallback(
+        () => setShowMailchimp((showMailchimp) => !showMailchimp),       
+        []);
+
+    const mailchimpMarkup = (
+        showMailchimp ? (
+            <Grid>
+                <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 3, lg: 6, xl: 6}}>           
+                    <Card.Section>
+                        {mailchimpEnabled.data?.map((enabled, i) => (
+                            <Checkbox
+                                id={enabled.id}
+                                key={enabled.id}
+                                position={i}
+                                label="Enabled"
+                                checked={mailchimpChecked}
+                                onChange={() => saveMailchimpCheck(enabled.id, enabled.enabled)}
+                            />                                
+                        ))}
+                    </Card.Section>  
+                </Grid.Cell>
+                <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 3, lg: 6, xl: 6}}>              
+                    
+                    <Card.Section>
+                    {mailchimpEnabled.data?.map((info, i) => (
+                    <Form 
+                        id={info.id}
+                        key={info.id}
+                        position={i}
+                        onSubmit={() => saveMailchimpInfo()}>
+                        <FormLayout>
+                            <TextField
+                                value={mailchimpApiKey}
+                                label="API Key"
+                                placeholder={info.apiKey}
+                                onChange={handleMailchimpApiKeyChange}
+                                autoComplete='off'
+                            />
+                            <TextField
+                                value={mailchimpServer}
+                                label="Server"
+                                placeholder={info.server}
+                                onChange={handleMailchimpServerIdChange}
+                                autoComplete='off'
+                            />
+                            <Button primary submit>Submit</Button>
+                        </FormLayout>
+                    </Form>
+                    ))}
+                    
+                    </Card.Section>                
+                </Grid.Cell>
+            </Grid>
+        ):( null )
     )
 
 
     return (
 
         <Page title="Integrations">
-            
+            <CalloutCard
+                illustration={ shopifyLogo }
+                primaryAction={{
+                    content: 'Shopify Integrations',
+                    onAction: () => enableShopify()
+                  }} 
+            >
+            {shopifyMarkup}    
+            </CalloutCard>
             
             <CalloutCard
                 illustration={ klaviyoLogo }
@@ -230,37 +388,20 @@ export default function IntegrationsPage() {
                     content: 'Klaviyo Integrations',
                     onAction: () => enableKlaviyo()
                   }} 
-                      
             >
-
-                {klaviyoMarkup}    
-                    
+            {klaviyoMarkup}  
             </CalloutCard>
-         
-            
-            <Card
-                title="Shopify"
+
+            <CalloutCard
+                illustration={ mailchimpLogo }
+                primaryAction={{
+                    content: 'Mailchimp Integrations',
+                    onAction: () => enableMailchimp()
+                  }} 
             >
-                        <Grid>
-                            <Grid.Cell columnSpan={{xs: 6, sm: 3, md: 3, lg: 6, xl: 6}}>
-                                <Card.Section>
-                                    <TextContainer>
-                                        {shopifyEnabled.data?.map((enabled, i) => (
-                                            <Checkbox
-                                                id={enabled.id}
-                                                key={enabled.id}
-                                                position={i}
-                                                label="Enabled"
-                                                checked={shopifyChecked}
-                                                onChange={() => saveShopifyCheck(enabled.id, enabled.enabled)}
-                                            />                                
-                                        ))}
-                                    </TextContainer>
-                                </Card.Section>                         
-                            </Grid.Cell>   
-                        </Grid>
-                </Card>
-          
+            {mailchimpMarkup}  
+            </CalloutCard>                       
+                           
         </Page>
 
     )
